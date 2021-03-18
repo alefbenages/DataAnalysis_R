3f2b8d2918c8e43e9c29c9c93edd954f28483024
AfBenages <alefbenages@gmail.com>
2021-03-18 21:01:15 +0100

    first commit

---
+++ GetStarted/Cars/Iris_Report.R
@@ -0,0 +1,126 @@
+#' ---
+#' title: "Iris Analysis"
+#' author: "Alejandro Fernandez Benages"
+#' date: "17/03/2021"
+#' ---
+#' 
+#' 
+
+
+#' **libraries**
+#install.packages('tidyverse')
+# install.packages(readr)
+library("readr")
+#library(tidyverse)
+library(ggplot2)
+
+#' **Load CSV**
+#' 
+setwd('/home/ale/Dropbox/UBIQUM/3. DA with R/1.GetStarted/Iris') #set wd
+IrisDataset <- read.csv('iris.csv')
+
+#' **Some info about the Data**
+#' 
+attributes(IrisDataset)
+summary(IrisDataset) 
+str(IrisDataset)
+names(IrisDataset)
+
+#' specific info about the Petal.Lenght 
+summary(IrisDataset$Petal.Length)
+#' specific info about the Petal.Width 
+summary(IrisDataset$Petal.Width)
+
+
+#' **Missing Values?** 
+#' 
+#' Summary will count how many NA’s you have.
+summary(IrisDataset)
+#' Anyway *is.na(dataset)* show your NA’s through logical data. (TRUE if it’s missing, FALSE if it’s not.)
+is.na(IrisDataset) 
+
+#' Replace the missing values with the mean, which is common technique, but   
+#' something to use with care with as it can skew the data.
+#DatasetName$ColumnName[is.na(DatasetName$ColumnName)]<-mean(DatasetName$ColumnName,na.rm = TRUE)
+
+#' **Some Plots**
+#'
+#' PETAL LENGTH
+hist(IrisDataset$Petal.Length, 
+     main="Sepal Length distribution", xlab="Lenght [mm]", 
+     col='green')
+     
+#' boxplot
+ggplot(data = IrisDataset, aes(x = "", y = Petal.Length)) + 
+  geom_boxplot() + theme_classic()+ 
+  xlab("")+ylab("Petal Lenght [mm]")
+
+#' violinplot
+ggplot(data = IrisDataset, aes(x = "", y = Petal.Length)) + 
+  geom_violin() + theme_classic()+ 
+  xlab("")+ylab("Petal Lenght [mm]")+ 
+  geom_boxplot(width=0.1)+ 
+  stat_summary(fun.y=median, geom="point", size=2, color="red") 
+
+
+#' PETAL WIDTH
+hist(IrisDataset$Petal.Width, 
+     main="Sepal Length distribution", xlab="Lenght [mm]", 
+     col='violet')
+
+#' boxplot
+ggplot(data = IrisDataset, aes(x = "", y = Petal.Width)) + 
+  geom_boxplot() + theme_classic()+ 
+  xlab("")+ylab("Petal Lenght [mm]")
+
+#' violinplot
+ggplot(data = IrisDataset, aes(x = "", y = Petal.Width)) + 
+  geom_violin() + theme_classic()+ 
+  xlab("")+ylab("Petal Lenght [mm]")+ 
+  geom_boxplot(width=0.1)+ 
+  stat_summary(fun.y=median, geom="point", size=2, color="red") 
+
+qqnorm(IrisDataset$Sepal.Length)
+
+#' SPECIES distributon
+IrisDataset$Species<- as.numeric(IrisDataset$Species) # changes the type to numeric
+hist(IrisDataset$Species,
+     main="Species distribution", xlab="Species",
+     col='darkmagenta')  
+
+#' Scatter plot Petal Length vs Petal Width
+ggplot(data = IrisDataset, aes(x = Petal.Length  , y = Petal.Width))  + 
+  geom_point(shape = 1) + 
+  xlab("Petal Lenght [mm]")+ylab("Petal Width [mm]") +
+  theme_bw()
+
+
+
+#' **Train/Test Split**
+#'
+set.seed(123)
+#' set dimension of Train/Test vectors
+trainSize <- round(nrow(IrisDataset) * 0.2)
+testSize <- nrow(IrisDataset) - trainSize
+#' just to check, print the dimensions 
+trainSize
+testSize
+#' assign the values to empty vectors  
+training_indices<-sample(seq_len(nrow(IrisDataset)),size =trainSize)   
+trainSet <- IrisDataset[training_indices, ]
+testSet <- IrisDataset[-training_indices, ]
+
+
+#' **Linear Model**
+#' 
+#' The analysis goal is to predict a petal's length using the petal’s width.  
+#' Inp Var -> petal’s width
+#' Out Var -> petal's length
+
+LinearModel<- lm(Petal.Width ~ Petal.Length, trainSet )
+
+summary(LinearModel)
+     
+prediction<-predict(LinearModel)
+     
+prediction
---
+++ GetStarted/Cars/cars.R
@@ -0,0 +1,132 @@
+#' ---
+#' title: "Cars Analysis"
+#' author: "Alejandro Fernandez Benages"
+#' date: "17/03/2021"
+#' ---
+
+#' **libraries**
+#install.packages('tidyverse')
+#install.packages(readr)
+library("readr")
+library(ggplot2)
+
+
+#' **Load CSV**
+#' 
+setwd('/home/ale/Dropbox/UBIQUM/3. DA with R/1.GetStarted/Cars')
+IrisDataset <- read.csv('cars.csv')
+
+
+# **1. EDA** 
+#' 
+## **1.01 Knowing the data**
+#'  
+#' To list the attributes of the data set.
+attributes(cars)
+#' To print the min, max, mean, median, and quartiles of each attribute.
+summary(cars)
+#' To display the structure of your data set.
+str(cars) 
+#' To get th names of the attributes within your data set.
+names(cars) 
+
+
+
+#'**1.2 Some Plotting**
+#' 
+
+#' SPEED
+#'A Histogram reveals that the most frequent speed of cars is between 10 to 20. 
+hist(cars$speed, 
+     main="Speed distribution", xlab="Speed [mph]") 
+
+#' STOPPING DISTANCE
+#' The most frequent distance of most cars is between 20 to 40. 
+hist(cars$dist, 
+     main="Stopping distance distribution", xlab="Stopping Distance [feet]")
+
+ggplot(data = cars, aes(x = "", y = dist)) + 
+  geom_boxplot()+ theme_classic()+ 
+  xlab("")+ylab("Stopping Distance [feet]")
+
+ggplot(data = cars, aes(x = "", y = dist)) + 
+  geom_violin()+ theme_classic()+ 
+  xlab("")+ylab("Stopping Distance [feet]")
+
+
+#' **Normal Quantile Plot** is a way to see if your data is normally distributed.
+#' 
+#' We can see that there is a positive correlation btw Speed and Distance
+qqnorm(cars$speed) 
+qqnorm(cars$dist)
+
+
+#' Scatter plot of two Variables
+plot(cars, col='blue', pch=20, cex=2, 
+     main="Speed and Stopping Distance for 50 Cars", 
+     xlab="Speed [mph]", ylab="Stopping Distance [feet]")
+
+
+#' **Missing Values?** 
+
+#' Will count how many NA’s you have.
+summary(cars)
+#' Will show your NA’s through logical data. (TRUE if it’s missing, FALSE if it’s not.)
+is.na(cars) 
+
+#' Replace the missing values with the mean, which is common technique, but   
+#' something to use with care with as it can skew the data.
+#DatasetName$ColumnName[is.na(DatasetName$ColumnName)]<-mean(DatasetName$ColumnName,na.rm = TRUE)
+
+
+
+#' Creating Testing and Training Sets
+
+set.seed(122)
+
+# These two lines calculate the sizes of each set but do not create the sets:
+trainSize<-round(nrow(cars)*0.7) # 70/30%
+testSize<-nrow(cars)-trainSize
+
+# If you’d like to see how many instances will be in each set..
+trainSize
+testSize
+
+#' How do you create the train/test sets?. We also want these sets to be in a 
+#' randomized order, which will create the most optimal model.
+#'   To perform this, you need to run these three lines of code. 
+training_indices<-sample(seq_len(nrow(cars)),size =trainSize)
+trainSet<-cars[training_indices,]
+testSet<-cars[-training_indices,]
+
+
+
+#' Linear Regression
+#' 
+#' 
+#' The next step is to predict the cars distances through the speed of the cars. 
+#' To do this, we’ll be using the prediction function – predict() 
+#' 
+#' Inp Var -> speed of the cars
+#' 
+#' Out Var -> cars distances
+
+ 
+# The basic line of code for the linear model function. 
+cars_regression <-lm(formula = dist ~ speed, trainSet)
+# To see key metrics of your model:
+summary(cars_regression)
+
+
+
+#' **Predictions**
+
+cars_predictions <- predict(cars_regression, testSet)
+cars_predictions
+
+#' Confidence in your predictions: In order to have an idea about the accuracy of 
+#' the predictions, you can ask for intervals around your prediction. To get a 
+#' matrix with the prediction and a 95 percent confidence interval around the mean 
+#' prediction, you set the argument interval to ‘confidence’ like this:
+predict(cars_regression,trainSet, interval='confidence')
+predict(cars_regression,testSet, interval='prediction')
