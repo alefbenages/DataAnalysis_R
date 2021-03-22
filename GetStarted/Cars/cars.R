b21fee33278d8a40495f4365b9aedd1c5baa5a37
AfBenages <alefbenages@gmail.com>
2021-03-22 11:43:57 +0100

    Added the RMD versions

--- GetStarted/Cars/cars.R
+++ GetStarted/Cars/cars.R
@@ -1,82 +1,94 @@
-#' ---
-#' title: "Cars Analysis"
-#' author: "Alejandro Fernandez Benages"
-#' date: "17/03/2021"
-#' ---
+---
+title: "Cars Analysis"
+author: "Alejandro Fernandez Benages"
+date: "17/03/2021"
+---
 
-#' **libraries**
+# Libraries
+```{r}
 #install.packages('tidyverse')
 #install.packages(readr)
 library("readr")
 library(ggplot2)
+```
 
-
-#' **Load CSV**
-#' 
-setwd('/home/ale/Dropbox/UBIQUM/3. DA with R/1.GetStarted/Cars')
+# Load CSV
+```{r}
 IrisDataset <- read.csv('cars.csv')
+```
 
-
-# **1. EDA** 
-#' 
-## **1.01 Knowing the data**
-#'  
-#' To list the attributes of the data set.
+# 1. EDA 
+## 1.1 Knowing the data
+```{r}
+# To list the attributes of the data set.
 attributes(cars)
-#' To print the min, max, mean, median, and quartiles of each attribute.
+# To print the min, max, mean, median, and quartiles of each attribute.
 summary(cars)
-#' To display the structure of your data set.
+# To display the structure of your data set.
 str(cars) 
-#' To get th names of the attributes within your data set.
+# To get th names of the attributes within your data set.
 names(cars) 
+```
 
-
-
-#'**1.2 Some Plotting**
-#' 
-
-#' SPEED
-#'A Histogram reveals that the most frequent speed of cars is between 10 to 20. 
+## 1.2 Some Plots
+### Variable: SPEED
+```{r}
 hist(cars$speed, 
      main="Speed distribution", xlab="Speed [mph]") 
+```
+A Histogram reveals that the most frequent speed of cars is between 10 to 20. 
 
-#' STOPPING DISTANCE
-#' The most frequent distance of most cars is between 20 to 40. 
+### Variable: STOPPING DISTANCE
+```{r}
 hist(cars$dist, 
      main="Stopping distance distribution", xlab="Stopping Distance [feet]")
-
+```
+The most frequent distance of most cars is between 20 to 40. 
+```{r}
 ggplot(data = cars, aes(x = "", y = dist)) + 
   geom_boxplot()+ theme_classic()+ 
   xlab("")+ylab("Stopping Distance [feet]")
-
+```
+A histogram doesn't reveal much data. 
+```{r}
 ggplot(data = cars, aes(x = "", y = dist)) + 
   geom_violin()+ theme_classic()+ 
   xlab("")+ylab("Stopping Distance [feet]")
+```
+But a Violin plot does!. Now we have a much clear idea of the distribution's shape. 
 
-
-#' **Normal Quantile Plot** is a way to see if your data is normally distributed.
-#' 
-#' We can see that there is a positive correlation btw Speed and Distance
+## Normal Quantile Plot is a way to see if your data is normally distributed.
+```{r}
 qqnorm(cars$speed) 
 qqnorm(cars$dist)
+```
+We can see that there is a positive correlation btw Speed and Distance
 
-
-#' Scatter plot of two Variables
+### Scatter plot of Speed vs Distance
+```{r}
 plot(cars, col='blue', pch=20, cex=2, 
      main="Speed and Stopping Distance for 50 Cars", 
      xlab="Speed [mph]", ylab="Stopping Distance [feet]")
+```
 
+## Missing Values?
 
-#' **Missing Values?** 
-
-#' Will count how many NA’s you have.
+If there are, the summarye() will count how many NA’s you have.
+```{r}
 summary(cars)
-#' Will show your NA’s through logical data. (TRUE if it’s missing, FALSE if it’s not.)
+```
+Also you can show your NA’s through logical data. (TRUE if it’s missing, FALSE if it’s not.)
+```{r}
 is.na(cars) 
-
-#' Replace the missing values with the mean, which is common technique, but   
-#' something to use with care with as it can skew the data.
+```
+Replace the missing values with the mean, which is common technique, but **something to use with care with as it can skew the data**.
+```{r}
 #DatasetName$ColumnName[is.na(DatasetName$ColumnName)]<-mean(DatasetName$ColumnName,na.rm = TRUE)
+```
+
+
+```{r}
+
 
 
 
@@ -130,3 +142,10 @@ cars_predictions
 #' prediction, you set the argument interval to ‘confidence’ like this:
 predict(cars_regression,trainSet, interval='confidence')
 predict(cars_regression,testSet, interval='prediction')
+
+
+
+
+
+```
+
